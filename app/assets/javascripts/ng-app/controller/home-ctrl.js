3166c5b6d7acc40fb84d369ee7b4eaa2009b0cd9
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-10 14:40:09 -0800

    retrieve the most recent tweet of the last 4 people the current user followed

--- app/assets/javascripts/ng-app/controller/home-ctrl.js
+++ app/assets/javascripts/ng-app/controller/home-ctrl.js
@@ -3,7 +3,7 @@ app.controller('HomeCtrl', ['$scope', 'UserFactory', 'SearchFactory',
     $scope.user = UserFactory.currentUser;
     $scope.tweets = SearchFactory.getResults.query();
     $scope.tweets.$promise.then(function(data){
-      $scope.test = data;
+      $scope.tweetData = data.ids;
     });
 }]);
 
