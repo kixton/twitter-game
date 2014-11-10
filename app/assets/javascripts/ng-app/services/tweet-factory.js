13b799db88751a7f2ecf86195167ce3f414fe4c1
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-10 15:48:46 -0800

    update tweets view to display the user names of the last 4 people I followed

--- app/assets/javascripts/ng-app/services/tweet-factory.js
+++ app/assets/javascripts/ng-app/services/tweet-factory.js
@@ -2,7 +2,6 @@ app.factory('TweetFactory', ['$resource', function($resource) {
   var searchRsc = $resource("/tweets");
 
   return {
-    getResults: searchRsc.get(),
-    userNames: Object.getOwnPropertyNames(searchRsc.get())
+    getResults: searchRsc.get()
   };
 }]);
