2a69e28dc4a1d07fa16bf0a10c74e602dce21d80
Stephan Yu <Stephan.yu@gmail.com>
2014-11-12 14:02:50 -0800

    added showHighScore and showMenu button/functions to menuCtrl and created basic table structure in menu.html

--- app/assets/javascripts/ng-app/controller/menu-ctrl.js
+++ app/assets/javascripts/ng-app/controller/menu-ctrl.js
@@ -1,4 +1,12 @@
 app.controller('MenuCtrl', ['$scope', "GameFactory",
   function($scope, GameFactory) {
     $scope.startGame = GameFactory.startGame
+    $scope.view = true
+    $scope.scores
+    $scope.showHighScore = function() {
+      $scope.view = false;
+    };
+    $scope.showMenu = function() {
+      $scope.view = true;
+    };
 }]);
