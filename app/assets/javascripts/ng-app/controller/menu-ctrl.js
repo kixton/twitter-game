9d12b9e0a5c54cbc4dd02f9ef1166ed477653fdf
Stephan Yu <Stephan.yu@gmail.com>
2014-11-13 11:22:41 -0800

    added currentUser high scores

--- app/assets/javascripts/ng-app/controller/menu-ctrl.js
+++ app/assets/javascripts/ng-app/controller/menu-ctrl.js
@@ -1,14 +1,20 @@
-app.controller('MenuCtrl', ['$scope', "GameFactory", "ScoreFactory",
-  function($scope, GameFactory, ScoreFactory) {
+app.controller('MenuCtrl', ['$scope', "GameFactory", "ScoreFactory", "UserFactory",
+  function($scope, GameFactory, ScoreFactory, UserFactory) {
     $scope.startGame = GameFactory.startGame
     $scope.view = true
     ScoreFactory.allScores.query().$promise.then(function(data) {
       $scope.scores = data
+    });
+    UserFactory.currentUser.$promise.then(function(data) {
+      $scope.currentUser = data
     });
     $scope.showHighScore = function() {
       $scope.view = false;
     };
     $scope.showMenu = function() {
       $scope.view = true;
+    };
+    $scope.showGraph = function() {
+      
     };
 }]);
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,7 +4,7 @@ class ScoresController < ApplicationController
 
     scores_array = []
     @scores.each do |score|
-      scores_array << { scores: score.scores, user: User.find(score.user_id).full_name }
+      scores_array << { scores: score.scores, user: User.find(score.user_id).full_name, user_id: score.user_id, created_at: score.created_at }
     end
 
     render json: scores_array
