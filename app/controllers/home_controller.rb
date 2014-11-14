a0bb0e634071644d7d72b444bd8a371048cdc4f8
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-14 14:11:57 -0800

    add access tokens in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,8 @@ class HomeController < ApplicationController
     Twitter::REST::Client.new do |config|
       config.consumer_key        = Rails.application.secrets.twitter_api_key      # "YOUR_CONSUMER_KEY"
       config.consumer_secret     = Rails.application.secrets.twitter_api_secret   # "YOUR_CONSUMER_SECRET"
+      config.access_token        = Rails.application.secrets.twitter_access_token
+      config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
     end
   end
 
