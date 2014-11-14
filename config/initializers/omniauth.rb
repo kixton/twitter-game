cc741b838b129c9b74dc3ef8582e38693f8b6695
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-14 15:34:20 -0800

    update omniauth.rb to include keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
-  provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"]
+  provider :twitter, "1eflVHwPlh9xu29TdPbSPZqDS", "tjr5yp6gC7OedapbAnEkPNfxjsS4tgSE73A80geF7Yd3hjelvt"
 end
