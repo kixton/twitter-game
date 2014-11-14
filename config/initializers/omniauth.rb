e5123b4220447d6307563fa9a4f108928520f190
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-14 15:50:06 -0800

    hide api key in omniauth.rb file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
-  provider :twitter, "1eflVHwPlh9xu29TdPbSPZqDS", "tjr5yp6gC7OedapbAnEkPNfxjsS4tgSE73A80geF7Yd3hjelvt"
+  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end
